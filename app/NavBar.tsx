5f45ffb9555cf4fc65dac25b97e704a527603e7a
hakehardware <alex@alexhake.com>
2024-08-21 14:17:31 +0000

    styled active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,34 @@
-import { La_Belle_Aurore } from 'next/font/google'
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
 
 const NavBar = () => {
+    const currentPath = usePathname()
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
     ]
+
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
             <Link href="/">
                 <AiFillBug />
             </Link>
+
             <ul className="flex space-x-6">
                 {links.map((link) => (
                     <Link
                         key={link.label}
-                        className="text-zinc-500 hover:text-zinc-800 transition-colors"
+                        className={classnames({
+							'text-zinc-900': link.href === currentPath,
+							'text-zinc-500': link.href !== currentPath,
+							'hover:text-zinc-800 transition-colors': true
+						})}
                         href={link.href}
                     >
                         {link.label}
