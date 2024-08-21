761961755dc41e25bbfee61908e009476584ff0d
hakehardware <alex@alexhake.com>
2024-08-21 22:12:49 +0000

    set up radix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './NavBar'
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +21,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <NavBar />
-                <main>{children}</main>
+                <Theme>
+                    <NavBar />
+                    <main>{children}</main>
+                </Theme>
             </body>
         </html>
     )
