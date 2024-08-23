2da7e4eb1e167ae8cc45f48586718499c94bfc0d
hakehardware <alex@alexhake.com>
2024-08-23 23:28:57 +0000

    edit page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,9 @@
-import { Box } from '@radix-ui/themes'
 import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import IssueForm from '../_components/IssueForm'
 
 const NewIssuePage = () => {
   return (
-    <Box className='max-w-xl'>
-      <Skeleton />
-      <Skeleton height="20rem" />
-    </Box>
+	<IssueForm />
   )
 }
 
