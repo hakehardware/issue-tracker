5daea7bb122860ececfafe26ba968b7606ac94cf
hakehardware <alex@alexhake.com>
2024-08-23 13:25:52 +0000

    show issue details

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,71 +1,9 @@
-'use client'
-
-import { Button, Callout, Text, TextField } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor'
-import 'easymde/dist/easymde.min.css'
-import { useForm, Controller } from 'react-hook-form'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/validationSchemas'
-import { z } from 'zod'
-import ErrorMessage from '@/app/components/ErrorMessage'
-import Spinner from '@/app/components/Spinner'
-
-// Allows us to use the createIssueSchemea to define the IssueForm
-type IssueForm = z.infer<typeof createIssueSchema>
+import React from 'react'
 
 const NewIssuePage = () => {
-    const {
-        register,
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<IssueForm>({
-        resolver: zodResolver(createIssueSchema),
-    })
-    const router = useRouter()
-    const [error, setError] = useState('')
-    const [submitting, setSubmitting] = useState(false)
-
-    const onSubmit = handleSubmit(async (data) => {
-        try {
-            setSubmitting(true)
-            await axios.post('/api/issues', data)
-            router.push('/issues')
-        } catch (error) {
-            setSubmitting(false)
-            setError('An unexpected error ocurred.')
-        }
-    })
-
-    return (
-        <div className="max-w-xl">
-            <form
-                className="space-y-3"
-                onSubmit={onSubmit}
-            >
-                <TextField.Root
-                    placeholder="Title"
-                    {...register('title')}
-                ></TextField.Root>
-                <ErrorMessage>{errors.title?.message}</ErrorMessage>
-                <Controller
-                    name="description"
-                    control={control}
-                    render={({ field }) => (
-                        <SimpleMDE placeholder="Description" {...field} />
-                    )}
-                />
-                <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
-                <Button disabled={submitting}>
-                    {submitting == false ? 'Submit Issue' : <Spinner />}
-                </Button>
-            </form>
-        </div>
-    )
+  return (
+    <div>NewIssuePage</div>
+  )
 }
 
 export default NewIssuePage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -12,7 +12,6 @@ const IssuesPage = async () => {
 
     return (
         <div>
-
             <IssueActions />
             <Table.Root variant="surface">
                 <Table.Header>
@@ -30,10 +29,14 @@ const IssuesPage = async () => {
                     {issues.map((issue) => (
                         <Table.Row key={issue.id}>
                             <Table.Cell>
-                                {issue.title}{' '}
-                                <div className="block md:hidden">
-                                    <IssueStatusBadge status={issue.status} />
-                                </div>
+                                <Link href={`/issues/${issue.id}`}>
+                                    {issue.title}{' '}
+                                    <div className="block md:hidden">
+                                        <IssueStatusBadge
+                                            status={issue.status}
+                                        />
+                                    </div>
+                                </Link>
                             </Table.Cell>
                             <Table.Cell className="hidden md:table-cell">
                                 <IssueStatusBadge status={issue.status} />
