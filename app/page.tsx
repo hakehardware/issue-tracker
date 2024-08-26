74c1fe0bebdd291a55c9d58d3eab1be61d89d400
hakehardware <alex@alexhake.com>
2024-08-26 14:54:51 +0000

    build issue summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+import prisma from '@/prisma/db'
 import Pagination from './components/Pagination'
+import IssueSummary from './IssueSummary'
 import LatestIssues from './LatestIssues'
 
-export default function Home() {
+export default async function Home() {
+
+	const open = await prisma.issue.count({
+		where: {status: 'OPEN'}
+	})
+
+	const inProgress = await prisma.issue.count({
+		where: {status: 'IN_PROGRESS'}
+	})
+
+	const closed = await prisma.issue.count({
+		where: {status: 'CLOSED'}
+	})
+
     return (
         <div>
             <LatestIssues />
+			<IssueSummary open={open} inProgress={inProgress} closed={closed} />
         </div>
     )
 }
