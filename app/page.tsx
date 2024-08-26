b59a5feaa7fb7128226fc91e5cb56e631169f441
hakehardware <alex@alexhake.com>
2024-08-26 14:39:35 +0000

    build latest issue comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Pagination from './components/Pagination'
+import LatestIssues from './LatestIssues'
 
-export default function Home({
-    searchParams,
-}: {
-    searchParams: { page: string }
-}) {
-    return <Pagination issueCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+export default function Home() {
+    return (
+        <div>
+            <LatestIssues />
+        </div>
+    )
 }
